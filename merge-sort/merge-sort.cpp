5bc262b9871e0c56c854e3d8732b6c509b311c71
Levrero <f.levrero.florencio@gmail.com>
2020-05-02 16:03:13 +0100

    merge-sort finished

--- merge-sort/merge-sort.cpp
+++ merge-sort/merge-sort.cpp
@@ -1,18 +1,83 @@
 #include <iostream>
+#include <vector>
+
+// merge function
+std::vector<int> merge(const std::vector<int> unsorted_vector_1, const std::vector<int> unsorted_vector_2)
+{
+    int size_1 = unsorted_vector_1.size();
+    int size_2 = unsorted_vector_2.size();
+    int total_size = size_1 + size_2;
+
+    std::vector<int> sorted_vector(total_size);
+
+    int ii = 0, jj = 0;
+    for (int kk = 0; kk < total_size; ++kk)
+    {
+        if (ii >= size_1)
+        {
+            sorted_vector[kk] = unsorted_vector_2[jj];
+            ++jj;
+        }
+        else if (jj >= size_2)
+        {
+            sorted_vector[kk] = unsorted_vector_1[ii];
+            ++ii;
+        }
+        else if (unsorted_vector_1[ii] < unsorted_vector_2[jj])
+        {
+            sorted_vector[kk] = unsorted_vector_1[ii];
+            ++ii;
+        }
+        else
+        {
+            sorted_vector[kk] = unsorted_vector_2[jj];
+            ++jj;
+        }
+    }
+
+    return sorted_vector;
+}
+
+// merge-sort function
+std::vector<int> merge_sort(const std::vector<int> unsorted_vector)
+{
+    int n = unsorted_vector.size();
+
+    if (n <= 1)
+    {
+        return unsorted_vector;
+    }
+    else
+    {
+        std::vector<int> unsorted_vector_1 = std::vector<int>(unsorted_vector.begin(), unsorted_vector.begin() + n / 2);
+        std::vector<int> unsorted_vector_2 = std::vector<int>(unsorted_vector.begin() + n / 2, unsorted_vector.end());
+
+        std::vector<int> sorted_vector_1 = merge_sort(unsorted_vector_1);
+        std::vector<int> sorted_vector_2 = merge_sort(unsorted_vector_2);
+
+        return merge(sorted_vector_1, sorted_vector_2);
+    }
+}
 
 // main function
 int main()
 {
-    // initialise the array to a random value
-    int array_size = 100;
-    std::array<int, array_size> unsorted_array;
-    for (auto &element : unsorted_array)
+    // initialise the vector to a random value
+    const int vector_size = 100;
+    std::vector<int> unsorted_vector(vector_size);
+    for (int ii = 0; ii < vector_size; ++ii)
     {
-        element = rand() % array_size + 1;
+        unsorted_vector[ii] = rand() % vector_size + 1;
     }
 
     // sort the elements of the array
-    auto sorted_array = merge_sort(unsorted_array);
-}
+    auto sorted_array = merge_sort(unsorted_vector);
 
-// merge-sort algorithm
+    // output results
+    for (int ii = 0; ii < vector_size; ++ii)
+    {
+        std::cout << "Sorted array[" << ii << "] is " << sorted_array[ii] << std::endl;
+    }
+
+    return 0;
+}
